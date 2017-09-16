95cd4f0836df5d7b99c43ab1b379079abe266d86
vmiller <vmiller@hostileadmin.com>
2017-09-15 22:52:28 -0400

    Implement loop with an exit.

--- age.c
+++ age.c
@@ -3,6 +3,7 @@
 #undef LINT
 #define YOUNG 40
 #define OLD 70
+#define QUIT 200
 
 int getNum()
 {
@@ -22,16 +23,20 @@ int main(int argc, char *argv[])
       printf( "argv[%d]: %s\n", i, argv[i] );
    #endif
 
-   int age = getNum();
-   if ( age < YOUNG ) {
-      printf( "%d is less than %d\n", age, YOUNG );
-   }
-   else if ( age > OLD ) {
-      printf( "%d is more than %d\n", age, OLD );
-   }
-   else {
-      printf( "%d is middle-aged\n", age );
-   }
+   static int age;
+   do {
+      age = getNum();
+      if ( age < YOUNG ) {
+         printf( "%d is less than %d\n", age, YOUNG );
+      }
+      else if ( age > OLD ) {
+         printf( "%d is more than %d\n", age, OLD );
+      }
+      else {
+         printf( "%d is middle-aged\n", age );
+      }
+
+   } while( age != QUIT );
 
    return 0;
 }
