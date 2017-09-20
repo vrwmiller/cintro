25d4ddc540904bca06f5662c8b6d51f7c36e12be
vmiller <vmiller@hostileadmin.com>
2017-09-19 21:00:17 -0400

    Implement alternating player framework.  This code results in an endless loop.

--- ttt.c
+++ ttt.c
@@ -9,13 +9,45 @@ char board[3][3] = {
    { '-','-','-' },
    { '-','-','-' },
 };
-int o, x;
+char player = 'X';
 
 /*
  * Functions
  */
+int take_position()
+{
+/* 0 = board[0][0]
+ * 1 = board[0][1]
+ * 2 = board[0][2]
+ * 3 = board[1][0]
+ * 4 = board[1][1]
+ * 5 = board[1][2]
+ * 6 = board[2][0]
+ * 7 = board[2][1]
+ * 8 = board[2][2]
+ */
+
+   return 0;
+}
+
 int take_turn()
 {
+   int position;
+
+   printf("X, select a position [0-8]: ");
+   scanf("%d", &position); /* I really don't like character strings */
+
+   if( player == 'X' ) {
+      player = 'O';
+   }
+   else if( player == 'O' ) {
+      player = 'X';
+   }
+
+   #ifdef DEBUG
+   printf("Player = %c\n", player);
+   #endif
+
    return 0;
 }
 
@@ -50,19 +82,24 @@ int draw_board()
 int main(int argc, char *argv[])
 {
 
-   /* Some pre-processor code for very basic lint */
    #ifdef DEBUG
-
    static int i;
    printf( "argc: %d\n", argc );
    for( i=0; i<argc; i++ )
       printf( "argv[%d]: %s\n", i, argv[i] );
-
    #endif
 
-   printf("Welcome to Tic-Tac-Toe!\n\n");
+   printf("Welcome to Tic-Tac-Toe!\n");
+   printf("Positions on the board are 0 - 8 starting at top left, ending\n");
+   printf("bottom right.\n");
    draw_board(board);
-   printf("\nThe board is clear, X goes first!\n\n");
+   printf("\nThe board is clear, X goes first!\n");
+
+
+   while(1)
+   {
+      take_turn(player);
+   }
 
    return 0;
 }
